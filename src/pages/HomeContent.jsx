8f72a27b52e80f6933c5fe13e7aace01b7738697
spectra creative <creativespectra0@gmail.com>
2026-04-03 15:24:05 +0530

    Revert scrollytelling intro back to timed splash screen

--- src/pages/HomeContent.jsx
+++ src/pages/HomeContent.jsx
@@ -6,12 +6,10 @@ import Contact from './Contact';
 import Collaboration from './Collaboration';
 import BrandGallery from '../components/home/BrandGallery';
 import LogoDisplay from '../components/home/LogoDisplay';
-import ScrollIntro from '../components/home/ScrollIntro';
 
 const HomeContent = () => {
     return (
         <>
-            <ScrollIntro />
             <section id="home">
                 <Home />
             </section>
